74430091bb70cd76a1afdecad2ea5066075404aa
Nagiraud <nathangiraud123@gmail.com>
2025-03-02 13:22:26 +0100

    Update GreetingController.php

--- src/Controller/GreetingController.php
+++ src/Controller/GreetingController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Artist;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -63,9 +64,18 @@ class GreetingController extends AbstractController
         return $this->render('movies/show.html.twig');
     }
 
-    #[Route('/api/artist', name: 'app_event_show', requirements: ['id' => '\d+'])]
-    public function apiArtist(EntityManagerInterface $entityManager): Response
+    #[Route('/api/artist', name: 'api_artist', methods: ['GET'])]
+    public function getProducts(EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->render('movies/show.html.twig');
+        $repository = $entityManager->getRepository(Artist::class);
+        $movie = $repository->findAll();
+
+        $data = array_map(fn($movie) => [
+            'id' => $movie->getId(),
+            'name' => $movie->getName(),
+            'description' => $movie->getDescription(),
+        ], $movie);
+
+        return $this->json($data);
     }
 }
