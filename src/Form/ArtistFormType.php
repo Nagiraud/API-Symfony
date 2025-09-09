68331d328483e3872361ee43b3bf09c93a77e862
Nagiraud <nathangiraud123@gmail.com>
2025-09-09 19:07:23 +0200

    amélioration affichage des listes

--- src/Form/ArtistFormType.php
+++ src/Form/ArtistFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Artist;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,9 @@ class ArtistFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name',TextType::class, [
+                'label' => 'Nom',
+            ])
             ->add('description')
             ->add('image',FileType::class, [
                 'label' => 'Image (JPG, PNG)',
--- src/Form/EventFormType.php
+++ src/Form/EventFormType.php
@@ -6,6 +6,7 @@ use App\Entity\Artist;
 use App\Entity\Event;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,11 +15,14 @@ class EventFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name',TextType::class,[
+                "label" => "Nom",
+            ])
             ->add('date', null, [
                 'widget' => 'single_text',
             ])
             ->add('artist',EntityType::class,[
+                "label" => "Artiste",
                 'class' => Artist::class, //cette ligne du formulaire demande un artiste deja existant
                 'choice_label' => 'name', //affiche les nom des artiste dans chaque etiquette
                 'placeholder' => 'Choisissez un artiste',
